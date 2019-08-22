9391de99dd5d64c959bed76aecc63e25f3580552
SkyJin <skyjin@outlook.com>
2019-08-23 00:08:54 +0800

    安装好两个工具

---
+++ index.js
@@ -0,0 +1,23 @@
+const ioHook = require('iohook');
+const robot = require('robotjs');
+
+ioHook.on('keydown', event => {
+  console.log(event); // { type: 'mousemove', x: 700, y: 400 }
+});
+
+const desktopRight = ioHook.registerShortcut([29, 3675, 61005], (keys) => {
+    console.log('桌面向右')
+  });
+const desktopLeft = ioHook.registerShortcut([29, 3675, 61003], (keys) => {
+    console.log('桌面向左')
+  });
+// Register and start hook
+ioHook.start();
+
+// Alternatively, pass true to start in DEBUG mode.
+// ioHook.start(true);
+
+
+
+
+robot.keyTap("command");
