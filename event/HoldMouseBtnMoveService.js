c71210a170faf5f0cc3e84586af02a2f1515c5d5
SkyJin <skyjin@outlook.com>
2020-08-16 00:46:35 +0800

    feature: 支持不松开按钮继续触发

--- event/HoldMouseBtnMoveService.js
+++ event/HoldMouseBtnMoveService.js
@@ -4,6 +4,7 @@ const getMoveDirection = require('../util/getMoveDirection');
 const mousedown = 'mousedown';
 const mouseup = 'mouseup';
 const mousedrag = 'mousedrag';
+const mousemove = 'mousemove';
 const rightBtn = 2;
 const leftBtn = 1;
 const middleBtn = 3;
@@ -21,7 +22,9 @@ class HoldMouseBtnMoveService {
         this.downButton = 0;
         this.startPos = { x: -1, y: -1 };
         this.currentPos = { x: -1, y: -1 };
+        this.lastPos = {x: -1, y: -1};
         this.letItGo = false;
+        this.lastMoveDirection = null
 
         this._initListenerMap();
         this.start();
@@ -29,57 +32,128 @@ class HoldMouseBtnMoveService {
 
     _initListenerMap () {
         const listenerMap = this.listenerMap;
-        let { startPos, currentPos, ioHook, downButton, letItGo, buttonHandler } = this;
+        let { startPos, currentPos, lastPos ,letItGo, buttonHandler } = this;
 
         listenerMap.set(mousedown, event => {
-            console.log(event); 
+            // console.log(event); 
             if (letItGo) return;
 
-            downButton = event.button;// 记录按下的button
+            this.downButton = event.button;// 记录按下的button
         })
         listenerMap.set(mouseup, event => { 
             // console.log(event); 
             if (letItGo) { letItGo = false; return; }
             // 未移动鼠标时，触发真正的单击
-            if (startPos.x === -1 && startPos.y === -1) {
-                letItGo = true;
-                ioHook.enableClickPropagation();
-                robot.mouseClick(btn2RobotBtn[downButton]);
-                ioHook.disableClickPropagation();
-            }
+            // if (startPos.x === -1 && startPos.y === -1) {
+            //     letItGo = true;
+            //     ioHook.enableClickPropagation();
+            //     robot.mouseClick(btn2RobotBtn[downButton]);
+            //     ioHook.disableClickPropagation();
+            // }
 
-            downButton = 0;// 清除按下的button
-            // 重置位置变量
-            startPos.x = startPos.y = -1;
-            currentPos.x = currentPos.y = -1;
+            this._reset('full')
         })
         listenerMap.set(mousedrag, event => {
+            // console.log(event);
 
-            console.log(event);
-            if (buttonHandler.has(downButton)) {// 如果按钮被注册过了
+            if (buttonHandler.has(this.downButton)) {// 如果按钮被注册过了
+                if (this.lastMoveDirection) {
+                    if (lastPos.x === -1 && lastPos.y === -1) {
+                        lastPos.x = event.x;
+                        lastPos.y = event.y;
+                        return
+                    }
+                    
+                    currentPos.x = event.x;
+                    currentPos.y = event.y;
+                    if (startPos.x === -1 && startPos.y === -1) {
+                        switch(this.lastMoveDirection) {
+                            case 'up':
+                                if (currentPos.y > lastPos.y) {
+                                    startPos.x = event.x;
+                                    startPos.y = event.y;
+                                }
+                                break;
+                            case 'down':
+                                if (currentPos.y < lastPos.y) {
+                                    startPos.x = event.x;
+                                    startPos.y = event.y;
+                                }
+                                break;
+                            case 'left':
+                                if (currentPos.x > lastPos.x) {
+                                    startPos.x = event.x;
+                                    startPos.y = event.y;
+                                }
+                                break;
+                            case 'right':
+                                if (currentPos.x < lastPos.x) {
+                                    startPos.x = event.x;
+                                    startPos.y = event.y;
+                                }
+                                break;
+                        }
+                        lastPos.x = currentPos.x
+                        lastPos.y = currentPos.y
+
+                        return
+                    }
+                    let moveDirection = getMoveDirection(startPos, currentPos, this.lastMoveDirection);
+
+                    if (moveDirection) {
+                        console.log(moveDirection)
+                        // 执行事件handler
+                        buttonHandler.get(this.downButton)({ moveDirection });
+                        // 记录方向
+                        this.lastMoveDirection = moveDirection
+                        // 重置位置
+                        this._reset()
+                    }
+
+
+                    return
+                }
+
+                // 获取当前值
+                currentPos.x = event.x;
+                currentPos.y = event.y;
                 if (startPos.x === -1 && startPos.y === -1) {// 获取初始值
                     startPos.x = event.x;
                     startPos.y = event.y;
                     return;
-                } else {// 获取当前值
-                    currentPos.x = event.x;
-                    currentPos.y = event.y;
-                }
+                } 
                 let moveDirection = getMoveDirection(startPos, currentPos);
 
                 if (moveDirection) {
-                    console.log(typeof buttonHandler.get(downButton))
-                    buttonHandler.get(downButton)({ moveDirection });
-                    downButton = 0;
+                    console.log(moveDirection)
+                    // 执行事件handler
+                    buttonHandler.get(this.downButton)({ moveDirection });
+                    // 记录方向
+                    this.lastMoveDirection = moveDirection
+                    // 重置位置
+                    this._reset()
                 }
             }
         })
     }
 
+    _reset (type = 'part') {
+        let { startPos, currentPos, lastPos } = this;
+
+        if (type === 'full') {
+            this.downButton = 0;// 清除按下的button
+            this.lastMoveDirection = null; // 清除上一个移动方向，因为松开按钮之后就可以继续同一个方向了
+        }
+        // 重置位置变量以继续执行其他handler
+        startPos.x = startPos.y = -1;
+        currentPos.x = currentPos.y = -1;
+        lastPos.x = lastPos.y = -1;
+    }
+
     add (btnType, handler) {
         this.buttonHandler.set(btnType, handler);
 
-        this.ioHook.disableClickPropagation();
+        // this.ioHook.disableClickPropagation();
     }
 
     remove (btnType, handler) {
--- util/getMoveDirection.js
+++ util/getMoveDirection.js
@@ -3,24 +3,46 @@ const angle_135 = Math.PI / 4 + Math.PI / 2;
 const angle_225 = Math.PI / 4 + Math.PI;
 const angle_315 = Math.PI / 4 + Math.PI + Math.PI / 2;
 
-const getMoveDirection = (start, current) => {
+const getMoveDirection = (start, current, lastMoveDirection) => {
     const yLength = current.y - start.y;
     const xLength = current.x - start.x;
     const length = Math.sqrt(xLength * xLength + yLength * yLength);
     const cos = xLength / length;
+    let moveDirection = null;
 
-    if (length < 20) return null;
+    // 判断方向的逻辑
+    if (length < 40) return null;
     if (yLength >= 0) {
-        if (cos >= Math.cos(angle_45)) return 'right';
-        if (cos <= Math.cos(angle_45) && cos >= Math.cos(angle_135)) return 'up';
-        if (cos <= Math.cos(angle_135)) return 'left';
+        if (cos >= Math.cos(angle_45)) moveDirection = "right";
+        if (cos <= Math.cos(angle_45) && cos >= Math.cos(angle_135))
+            moveDirection = "down";
+        if (cos <= Math.cos(angle_135)) moveDirection = "left";
     } else {
-        if (cos <= Math.cos(angle_225)) return 'left';
-        if (cos > Math.cos(angle_225) && cos < Math.cos(angle_315)) return 'down';
-        if (cos >= Math.cos(angle_315)) return 'right';
+        if (cos <= Math.cos(angle_225)) moveDirection = "left";
+        if (cos > Math.cos(angle_225) && cos < Math.cos(angle_315))
+            moveDirection = "up";
+        if (cos >= Math.cos(angle_315)) moveDirection = "right";
     }
 
-    return null;
-}
+    // 拦截返回：如果触发过事件，但没放开按钮
+    if (lastMoveDirection) {
+        switch (lastMoveDirection) {
+            case "up":
+                if (moveDirection !== "down") return null;
+                break;
+            case "down":
+                if (moveDirection !== "up") return null;
+                break;
+            case "left":
+                if (moveDirection !== "right") return null;
+                break;
+            case "right":
+                if (moveDirection !== "left") return null;
+                break;
+        }
+    }
+
+    return moveDirection;
+};
 
 module.exports = getMoveDirection;
