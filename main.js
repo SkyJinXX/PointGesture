6bef8406ca18be59774016ab646ec0083eaf8999
SkyJin <skyjin@outlook.com>
2020-08-19 19:16:14 +0800

    add: 暂停功能时托盘图标变灰

--- main.js
+++ main.js
@@ -1,38 +1,47 @@
 const { app, Menu, Tray, dialog, globalShortcut } = require("electron");
 const path = require("path");
 const GestureService = require("./GestureService");
+
 let service, tray;
+const trayUrls = [
+    path.join(__dirname, "./assets/images/app.ico"),
+    path.join(__dirname, "./assets/images/app-gray.ico"),
+];
+let trayUrl = trayUrls[0];
 const menuTemplate = [
     {
         id: "toggle",
         label: "暂停",
-        click: () => app.toggle()
+        click: () => app.toggle(),
     },
     {
         label: "退出",
-        click: () => app.quit()
+        click: () => app.quit(),
     },
 ];
 app.toggle = () => {
     if (!service || !tray) {
-        return console.error('服务未启动');
+        return console.error("服务未启动");
     }
 
-    if(app.isActive) {
+    if (app.isActive) {
         service.pause();
         menuTemplate[0].label = "激活";
+        trayUrl = trayUrls[1];
         app.isActive = false;
     } else {
         service.active();
         menuTemplate[0].label = "暂停";
+        trayUrl = trayUrls[0];
         app.isActive = true;
     }
+    tray.setImage(trayUrl);
     tray.setContextMenu(Menu.buildFromTemplate(menuTemplate));
-}
+};
 
 app.on("ready", () => {
     service = new GestureService();
-    tray = new Tray(path.join(__dirname, "./assets/images/app.ico"));
+    tray = new Tray(trayUrl);
 
     tray.setToolTip("PointGesture");
     tray.setContextMenu(Menu.buildFromTemplate(menuTemplate));
